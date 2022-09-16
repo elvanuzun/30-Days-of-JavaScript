97de007fc246f636c85b030c4a26dae6fb84c225
Elvan Uzun <elvanuzun98@gmail.com>
2022-09-16 21:13:25 +0300

    Add day 1

---
+++ day01/datatypes.js
@@ -0,0 +1,9 @@
+//Welcome to 30DaysOfJavaScript
+/* Comments can make code readable
+Easy to reuse and informative
+*/
+console.log(typeof "hello")
+console.log(typeof 7)
+console.log(typeof false)
+console.log(typeof undefined)
+console.log(typeof null)
---
+++ day01/variables.js
@@ -0,0 +1,14 @@
+let firstName = "Monica"
+let lastName = "Geller"
+let age = 26
+let isMarried = false
+let salary = undefined
+let status = null 
+
+console.log(firstName,lastName,age)
+
+const PI = 3.14
+const gravity = 9.81
+
+console.log(PI,gravity)
+
